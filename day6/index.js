1c4afe624e6a5ede04c03fe73b1d466bfd2648d5
Soumya Kesharwani <kesharwanisoumya10@gmail.com>
2021-09-18 00:01:28 +0530

    day-6

---
+++ day6/index.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+var command = process.argv[2];
+var path = process.argv[3];
+var third_val = process.argv[4];
+
+console.log(command);
+console.log(path);
+console.log(third_val);
+
+switch (command) {
+  case "mkdir":
+    fs.mkdir(path);
+    break;
+
+  case "readfile":
+    console.log(fs.readFile(path, "utf-8"));
+    break;
+
+  case "writefile":
+    fs.writeFile(path, third_val);
+    break;
+
+  case "rename":
+    fs.rename(path, third_val);
+    break;
+
+  case "append":
+    fs.appendFileSync(path, third_val);
+    break;
+
+  case "unlink":
+    fs.unlink(path);
+    break;
+
+  case "rmdir":
+    fs.rmdir(path);
+    break;
+}
